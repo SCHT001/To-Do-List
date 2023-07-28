ac313b97c65f85a6d7b8d0a9d3017cf5136fde4e
SCHT001 <sachetsubedi001@gmail.com>
2023-07-28 17:41:58 +0545

    newToDoFIrstCommit

--- script.js
+++ script.js
@@ -1,89 +1,42 @@
-let inputBox=document.getElementById('input');
-let buttonAdder=document.getElementById('btn');
-let contain=document.getElementById('cnt');
-buttonAdder.addEventListener('click', elementAdder);
-
-function elementAdder()
-{
-    //delete button
-    let del=document.createElement('button');
-    del.addEventListener('click',elementDeleter);
-    let delImg=document.createElement('img');
-    delImg.src="image/dustbin.png";
-    delImg.style.height="15px";
-   delImg.style.marginLeft="5px";
-    del.appendChild(delImg);
-    del.style.backgroundColor="transparent";
-    del.style.border="none";
-
-    //mark button
-    let mark=document.createElement('button');
-    mark.addEventListener('click',elementMarker);
-    let markImg=document.createElement('img');
-    markImg.src="image/mark.png";
-    mark.appendChild(markImg);
-    markImg.style.height="15px";
-    mark.style.backgroundColor="transparent";
-    mark.style.border="none";
-
-    //div for delete and mark
-    let delAndMark=document.createElement('div');
-    delAndMark.style.display="flex";
-    delAndMark.appendChild(mark);
-    delAndMark.appendChild(del);
-    
-
-
-
-    let para=document.createElement('p');
-    para.innerText=inputBox.value;
-    var listBox=document.createElement('li');
-    
-    
-    contain.appendChild(listBox);
-    listBox.appendChild(para);
-    listBox.appendChild(delAndMark);
-    
-    inputBox.value="";
-    listBox.style.listStyle="none";
-    listBox.style.display="flex";
-    listBox.style.justifyContent="space-between";
-    
-    para.style.marginLeft="15px";
-
-
-
-
-    para.addEventListener('click',elementMarker);
-    para.addEventListener('dblclick',elementDeleter);
-
-
-    function elementMarker()
-    {
-        listBox.style.textDecoration="line-through";
-    }
-    function elementDeleter()
-    {
-        contain.removeChild(listBox);
-    }
-
-
-
-    
-}
-
-inputBox.addEventListener('keydown',function(event)
-{
-    if(event.keyCode===13)
-    {
-        elementAdder();
-    }
+var addBtn=document.getElementById('addBtn');
+var taskList=document.getElementById('taskLists');
+var inputContainer=document.getElementById("inputContainer");
+addBtn.addEventListener('click',()=>{
+    inputContainer.style.display="block";
+    inputContainer.style.top="0vh";
+});
+document.getElementById('cancelBtn').addEventListener('click',()=>{
+    inputContainer.style.top='-100vh';
+    clearInput();
 })
-let resetBtn=document.getElementById('clearBtn');
-resetBtn.addEventListener('click',resetAll);
-
-function resetAll()
-{
-    contain.innerHTML='';
+document.getElementById('createBtn').addEventListener('click',addTask);
+function clearInput(){
+    var title=document.getElementById('titleText');
+    var des=document.getElementById('desText');
+    title.value='';
+    des.value='';
 }
-
+function addTask(){
+    var currdate=new Date;
+    var mins=currdate.getMinutes();
+    var hrs=currdate.getHours();
+    var title=document.getElementById('titleText').value;
+    var des=document.getElementById('desText').value;
+    if(title==''||des==''){
+        alert("Please fill in the boxes");
+    }
+    else
+    {
+    var task=document.createElement('div');
+    task.innerHTML= `<div class="listItem">
+    <div class='sideDate'>${hrs} : ${mins}</div>
+    <div class='mainSide'>
+    <span class='titleSpan'>${title}</span>
+    <span class='desSpan'>${des}</span>
+    </div>
+    </div>`;
+    taskList.appendChild(task);
+    inputContainer.style.top='-100vh';
+    clearInput();
+    }
+}
