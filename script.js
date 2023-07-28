0ebffd9d4ce398e6b2182ab6453e39cc6223bae4
SCHT001 <sachetsubedi001@gmail.com>
2023-07-28 18:22:00 +0545

    deleteBtn

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function addTask(){
     var currdate=new Date;
     var mins=currdate.getMinutes();
     var hrs=currdate.getHours();
+    var sec=currdate.getSeconds();
     var title=document.getElementById('titleText').value;
     var des=document.getElementById('desText').value;
     if(title==''||des==''){
@@ -34,9 +35,33 @@ function addTask(){
     <span class='titleSpan'>${title}</span>
     <span class='desSpan'>${des}</span>
     </div>
-    </div>`;
+    <div class="sideBtn">
+    <button class='btn btn-primary'>Completed</button>
+    <button class='btn btn-danger deleteBtn' >Delete</button>
+    </div>
+    </div>`
+    
+    var obj=[
+        {
+            hours:hrs,
+            minutes:mins,
+            title:title,
+            description:des
+
+        }
+    ];
+    localStorage.setItem(Date.now(),JSON.stringify(obj));
     taskList.appendChild(task);
     inputContainer.style.top='-100vh';
     clearInput();
     }
+    var deleteBtn=document.querySelectorAll('.deleteBtn');
+    deleteBtn.forEach((btn)=>{
+    btn.addEventListener('click',deleteItem);
+})
+}
+
+function deleteItem(e){
+    const listItem=e.target.closest('.listItem');
+    listItem.remove();
 }
