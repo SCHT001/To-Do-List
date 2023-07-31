c33e295d79ecf1ef18fbfe40f51b8a1e40435050
nish33 <28823490+nish33@users.noreply.github.com>
2023-07-31 13:50:30 +0545

    v2

--- script.js
+++ script.js
@@ -1,140 +1,132 @@
-var addBtn=document.getElementById('addBtn');
-var taskList=document.getElementById('taskLists');
-var inputContainer=document.getElementById("inputContainer");
-addBtn.addEventListener('click',()=>{
-    inputContainer.style.display="block";
-    inputContainer.style.top="0vh";
+const state = {
+  tasks: [],
+  weekDay: ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"],
+};
+
+var addBtn = document.getElementById("addBtn");
+var taskList = document.getElementById("taskLists");
+var inputContainer = document.getElementById("inputContainer");
+addBtn.addEventListener("click", () => {
+  inputContainer.style.display = "block";
+  inputContainer.style.top = "0vh";
 });
-document.getElementById('cancelBtn').addEventListener('click',()=>{
-    inputContainer.style.top='-100vh';
-    clearInput();
-})
-document.getElementById('createBtn').addEventListener('click',addTask);
-function clearInput(){
-    var title=document.getElementById('titleText');
-    var des=document.getElementById('desText');
-    title.value='';
-    des.value='';
-} 
-const weekDay=[ 'Sun','Mon','Tue','Wed','Thur','Fri','Sat'];
-var taskArr=[];
-function addTask(){
-    var currdate=new Date;
-    var mins=currdate.getMinutes();
-    var hrs=currdate.getHours();
-    var week=currdate.getDay();
-    console.log(weekDay[week]);
-    var title=document.getElementById('titleText').value;
-    var des=document.getElementById('desText').value;
-    if(title==''||des==''){
-        alert("Please fill in the boxes");
-    }
-    else
-    {
-    var task=document.createElement('div');
-    task.innerHTML= `<div class="listItem">
-    <div class='sideDate'> 
-    <span class='weekDay'>${weekDay[week]}</span>
-    <span class='time'>${hrs}:${mins}</span> 
-    </div>
-    <div class='mainSide'>
-    <span class='titleSpan'>${title}</span>
-    <span class='desSpan'>${des}</span>
-    </div>
-    <div class="sideBtn">
-    <button class='btn btn-primary completedBtn'>Completed</button>
-    <button class='btn btn-danger deleteBtn' >Delete</button>
-    </div>
-    </div>`
-    
-    var obj=
-        [{
-            hours:hrs,
-            minutes:mins,
-            title:title,
-            description:des,
-            week:week,
-            status:0
-        }]
-    taskArr.push(obj);
-    localStorage.setItem("tasks",JSON.stringify(taskArr));
-    taskList.appendChild(task);
-    inputContainer.style.top='-100vh';
-    clearInput();
-    }
-    var deleteBtn=document.querySelectorAll('.deleteBtn');
-    deleteBtn.forEach((btn)=>{
-    btn.addEventListener('click',deleteItem);
+document.getElementById("cancelBtn").addEventListener("click", () => {
+  inputContainer.style.top = "-100vh";
+  clearInput();
+});
+document.getElementById("createBtn").addEventListener("click", addTask);
+function clearInput() {
+  var title = document.getElementById("titleText");
+  var des = document.getElementById("desText");
+  title.value = "";
+  des.value = "";
+}
 
-    var completedBtn=document.querySelectorAll('.completedBtn');
-    completedBtn.forEach((btn)=>{
-    btn.addEventListener('click',completedTask);
+function addTask() {
+  var currdate = new Date();
+  var mins = currdate.getMinutes();
+  var hrs = currdate.getHours();
+  var week = currdate.getDay();
+
+  var title = document.getElementById("titleText").value;
+  var des = document.getElementById("desText").value;
+  if (title == "" || des == "") {
+    alert("Please fill in the boxes");
+    return;
+  }
+
+  var obj = {
+    id: Date.now(),
+    hours: hrs,
+    minutes: mins,
+    title: title,
+    description: des,
+    week: week,
+    status: 0,
+  };
+  state.tasks.push(obj);
+
+  localStorage.setItem("tasks", JSON.stringify(state.tasks));
+
+  inputContainer.style.top = "-100vh";
+  clearInput();
+  getFromLocalStorage();
+  displayTasks();
+  return;
+}
+
+function deleteItem(id) {
+  var index = state.tasks.findIndex((item) => {
+    return item.id === id;
+  });
+  state.tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(state.tasks));
+  getFromLocalStorage();
+  return displayTasks();
+}
+function completeTask(id) {
+  const index = state.tasks.findIndex((item) => {
+    return item.id === id;
+  });
+  state.tasks[index].status = 1;
+  localStorage.setItem("tasks", JSON.stringify(state.tasks));
+  getFromLocalStorage();
+  return displayTasks();
+}
+function getFromLocalStorage() {
+  const tasks = localStorage.getItem("tasks");
+
+  state.tasks = tasks ? JSON.parse(tasks) : [];
+  return;
+}
+
+function displayTasks() {
+  const container = document.getElementById("taskLists");
+  container.innerHTML = "";
+  if (state.tasks.length > 0) {
+    state.tasks.forEach((task) => {
+      container.innerHTML += `
+        <div class="listItem" style="background: ${task.status === 1 ? 'green' : 'unset'}">
+            <div class='sideDate'>
+            <span class='weekDay'>${state.weekDay[task.week]}</span>
+            <span class='time'>${task.hours}:${task.minutes}</span>   </div>
+            <div class='mainSide'>
+            <span class='titleSpan'>${task.title}</span>
+            <span class='desSpan'>${task.description}</span>
+            </div>
+            <div class="sideBtn">
+            <button class='btn btn-primary completedBtn' onclick="completeTask(${task.id})">Completed</button>
+            <button class='btn btn-danger deleteBtn' onclick="deleteItem(${task.id})">Delete</button>
+            </div>
+        </div>
+        `;
     });
-})
+    return;
+  }
+  return (container.innerHTML = `<h3>No tasks yet.</h3>`);
 }
 
-function deleteItem(e){
-    const listItem=e.target.closest('.listItem');
-    listItem.remove();
-
-    const title=listItem.querySelector('.titleSpan').innerText;
-    const des=listItem.querySelector('.desSpan').innerText;
-    var taskFromStorage=JSON.parse(localStorage.getItem('tasks'));
-    var index= taskFromStorage.findIndex((item)=>{
-        return item[0].title===title && item[0].description===des;    
+document.getElementById("showAllBtn").addEventListener("click", () => {
+    getFromLocalStorage();
+    displayTasks();
+    return;
+});
+document.getElementById("showCompletedBtn").addEventListener("click", () => {
+    getFromLocalStorage();
+    state.tasks = state.tasks.filter(task => {
+        return task.status === 1;
     })
-    taskFromStorage.splice(index,1);
-    localStorage.setItem('tasks',JSON.stringify(taskFromStorage)); 
-}
-function completedTask(e){
-    const listItem=e.target.closest('.listItem');
-    listItem.style.backgroundColor='rgba(56, 245, 70, 0.3)';
-
-
-    const title=listItem.querySelector('.titleSpan').innerText;
-    const des=listItem.querySelector('.desSpan').innerText;
-    var taskFromStorage=JSON.parse(localStorage.getItem('tasks'));
-    var index= taskFromStorage.findIndex((item)=>{
-        return item[0].title===title && item[0].description===des;    
+    displayTasks();
+    return;
+});
+document.getElementById("showPendingBtn").addEventListener("click", () => {
+    getFromLocalStorage();
+    state.tasks = state.tasks.filter(task => {
+        return task.status === 0;
     })
-    
-    taskFromStorage[index][0].status=1;
-    localStorage.setItem('tasks',JSON.stringify(taskFromStorage));
-}
-function getFromLocalStorage(){
-    var taskItems=JSON.parse(localStorage.getItem("tasks"));
-    taskItems.forEach((t)=>{
-    var task=document.createElement('div');
-    task.innerHTML= `<div class="listItem">
-    <div class='sideDate'>
-    <span class='weekDay'>${weekDay[t[0].week]}</span>
-    <span class='time'>${t[0].hours}:${t[0].minutes}</span> </div>
-    <div class='mainSide'>
-    <span class='titleSpan'>${t[0].title}</span>
-    <span class='desSpan'>${t[0].description}</span>
-    </div>
-    <div class="sideBtn">
-    <button class='btn btn-primary completedBtn'>Completed</button>
-    <button class='btn btn-danger deleteBtn' >Delete</button>
-    </div>
-    </div>`
-    taskList.appendChild(task);
-    })
-    var deleteBtn=document.querySelectorAll('.deleteBtn');
-    deleteBtn.forEach((btn)=>{
-    btn.addEventListener('click',deleteItem);
-    });
-    var completedBtn=document.querySelectorAll('.completedBtn');
-    completedBtn.forEach((btn)=>{
-    btn.addEventListener('click',completedTask);
-    });
+    displayTasks();
+    return;
+});
 
-}
 getFromLocalStorage();
-
-function completedCheck(){
-    var taskItems=JSON.parse(localStorage.getItem("tasks"));
-    taskItems.find((e)=>{
-        e[0].status===0;
-    })
-}
+displayTasks();
